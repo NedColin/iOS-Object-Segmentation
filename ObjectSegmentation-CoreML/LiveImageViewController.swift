21cd1716de302a216c5dbd5e32bccd182da374c2
Ned <weixuegg@gmail.com>
2020-02-07 16:59:35 +0800

    merge

---
+++ ObjectSegmentation-CoreML/LiveImageViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ViewController.swift
+//  ImageSegmentation-CoreML
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class LiveImageViewController: UIViewController {
+
+    // MARK: - UI Properties
+    @IBOutlet weak var videoPreview: UIView!
+    @IBOutlet weak var drawingView: DrawingSegmentationView!
+    
+    @IBOutlet weak var inferenceLabel: UILabel!
+    @IBOutlet weak var etimeLabel: UILabel!
+    @IBOutlet weak var fpsLabel: UILabel!
+    
+    // MARK: - AV Properties
+    var videoCapture: VideoCapture!
+    
+    // MARK - Core ML model
+    // DeepLabV3(iOS12+), DeepLabV3FP16(iOS12+), DeepLabV3Int8LUT(iOS12+)
+    let segmentationModel = DeepLabV3()
+    
+    // MARK: - Vision Properties
+    var request: VNCoreMLRequest?
+    var visionModel: VNCoreMLModel?
+    
+    var isInferencing = false
+    
+    // MARK: - Performance Measurement Property
+    private let 👨‍🔧 = 📏()
+    
+    // MARK: - View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup ml model
+        setUpModel()
+        
+        // setup camera
+        setUpCamera()
+        
+        // setup delegate for performance measurement
+        👨‍🔧.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoCapture.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.videoCapture.stop()
+    }
+    
+    // MARK: - Setup Core ML
+    func setUpModel() {
+        if let visionModel = try? VNCoreMLModel(for: segmentationModel.model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            request?.imageCropAndScaleOption = .scaleFill
+        } else {
+            fatalError()
+        }
+    }
+    
+    // MARK: - Setup camera
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 50
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // UI에 비디오 미리보기 뷰 넣기
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // 초기설정이 끝나면 라이브 비디오를 시작할 수 있음
+                self.videoCapture.start()
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
+    }
+}
+
+// MARK: - VideoCaptureDelegate
+extension LiveImageViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?/*, timestamp: CMTime*/) {
+        
+        // the captured image from camera is contained on pixelBuffer
+        if let pixelBuffer = pixelBuffer, !isInferencing {
+            isInferencing = true
+            
+            // start of measure
+            self.👨‍🔧.🎬👏()
+            
+            // predict!
+            predict(with: pixelBuffer)
+        }
+    }
+}
+
+// MARK: - Inference
+extension LiveImageViewController {
+    // prediction
+    func predict(with pixelBuffer: CVPixelBuffer) {
+        guard let request = request else { fatalError() }
+        
+        // vision framework configures the input size of image following our model's input configuration automatically
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
+        try? handler.perform([request])
+    }
+    
+    // post-processing
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        self.👨‍🔧.🏷(with: "endInference")
+        
+        if let observations = request.results as? [VNCoreMLFeatureValueObservation],
+            let segmentationmap = observations.first?.featureValue.multiArrayValue {
+            
+            let segmentationResultMLMultiArray = SegmentationResultMLMultiArray(mlMultiArray: segmentationmap)
+            DispatchQueue.main.async { [weak self] in
+                // update result
+                self?.drawingView.segmentationmap = segmentationResultMLMultiArray
+                
+                // end of measure
+                self?.👨‍🔧.🎬🤚()
+                self?.isInferencing = false
+            }
+        } else {
+            // end of measure
+            self.👨‍🔧.🎬🤚()
+            isInferencing = false
+        }
+    }
+}
+
+// MARK: - 📏(Performance Measurement) Delegate
+extension LiveImageViewController: 📏Delegate {
+    func updateMeasure(inferenceTime: Double, executionTime: Double, fps: Int) {
+        //print(executionTime, fps)
+        self.inferenceLabel.text = "inference: \(Int(inferenceTime*1000.0)) mm"
+        self.etimeLabel.text = "execution: \(Int(executionTime*1000.0)) mm"
+        self.fpsLabel.text = "fps: \(fps)"
+    }
+}
